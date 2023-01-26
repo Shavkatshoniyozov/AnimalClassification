b48e013c9e72a7e13551da099026fbdb69970fd2
Shavkatshoniyozov <72923775+Shavkatshoniyozov@users.noreply.github.com>
2023-01-26 23:43:44 +0900

    Model has succesfully uploaded

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title("Animals' Classification Model")
+
+#Upload picture
+file = st.file_uploader("Upload picture", type=['png', 'jpeg', 'gif', 'svg', 'jpg'])
+if file:
+    st.image(file)
+
+    #PIL convert image
+    img =PILImage.create(file)
+
+    #Model
+    model = load_learner('animal1.pkl')
+
+    #Prediction
+    pred, pred_id, probs = model.predict(img)
+
+    #Print result
+    st.success(f"Prediction: {pred}")
+    st.info(f"Probability: {probs[pred_id]*100:.2f}%")
+
+    #Plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
